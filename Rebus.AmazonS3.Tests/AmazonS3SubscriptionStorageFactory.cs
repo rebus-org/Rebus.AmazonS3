db571b05af2b90d4e704569c51117be3a00f9578
Alexander Yaremchuk <tacklewit@gmail.com>
2017-10-11 13:24:56 +0200

    added operations test

---
+++ Rebus.AmazonS3.Tests/AmazonS3SubscriptionStorageFactory.cs
@@ -0,0 +1,25 @@
+﻿using Rebus.AmazonS3.AmazonS3;
+using Rebus.Logging;
+using Rebus.Subscriptions;
+using Rebus.Tests.Contracts.Subscriptions;
+
+namespace Rebus.AmazonS3.Tests
+{
+    public class AmazonS3SubscriptionStorageFactory : ISubscriptionStorageFactory
+    {
+        public ISubscriptionStorage Create()
+        {
+            var consoleLoggerFactory = new ConsoleLoggerFactory(true);
+            var storage = new AmazonS3SubscriptionsStorage(null, consoleLoggerFactory);
+
+            //storage.EnsureTableIsCreated();
+
+            return storage;
+        }
+
+        public void Cleanup()
+        {
+            //SqlTestHelper.DropTable(TableName);
+        }
+    }
+}
